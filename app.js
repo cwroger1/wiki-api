f38a28fb6bcce873c0b0e10e1b18c15c015c579f
cwroger1 <cwroger1@asu.edu>
2023-07-19 11:10:42 -0600

    Install ejs and use in app.js

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 
 const express = require('express');
+const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
+
+app.set('view engine', 'ejs');
 
 mongoose.connect('mongodb://127.0.0.1:27017/wiki-db')
 mongoose.connection.on('connected', function () {
     console.log('Mongoose default connection open to ' + 'mongodb://127.0.0.1:27017/wiki-db');
   });
 
-// app.use(express.static("public"));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000, () => {
